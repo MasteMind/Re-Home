8774370bb089807cf9133def84e30a3707b902fc
MasteMind <mehranramiz93@gmail.com>
2017-12-15 22:26:32 +0530

    ALL Configs

---
+++ startNgRok.sh
@@ -0,0 +1,7 @@
+/home/mastemind/Workspaces/portForwarding/ngrok tcp 22 -log=stdout > /home/mastemind/Workspaces/portForwarding/ngrok.log &
+sleep 20
+python printRandomUrl.py > randomLink.out
+git add randomLink.out
+now=$(date +"%T")
+git commit -m 'TIME:'+$(echo $now)
+git push -u https://mastemind:sh28461379@github.com/MasteMind/Re-Home.git master
