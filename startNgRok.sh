10584552a889c8c38de11cd52e7090cd2024fa79
mastemind <mehranramiz93@gmail.com>
2017-12-16 11:35:53 +0530

    TIME:+11:35:53

--- startNgRok.sh
+++ startNgRok.sh
@@ -1,8 +1,17 @@
+sleep 10
 cd /home/mastemind/Workspaces/portForwarding/
-/home/mastemind/Workspaces/portForwarding/ngrok tcp 22 -log=stdout > /home/mastemind/Workspaces/portForwarding/ngrok.log &
+echo "Printing th working directory!"
+pwd
+
+./ngrok start --all -config=/home/mastemind/.ngrok2/ngrok.yml -log=stdout>ngrok.log &
+
 sleep 20
 python printRandomUrl.py > randomLink.out
-git add randomLink.out
+
+git config --global user.email "mehranramiz93@gmail.com"
+git config --global user.name "mastemind"
+
+git add -A
 now=$(date +"%T")
 git commit -m 'TIME:'+$(echo $now)
 git push -u https://mastemind:sh28461379@github.com/MasteMind/Re-Home.git master
